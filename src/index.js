3ea16a140028c6fc71f483e483a44a422e567a70
fiftyloops <zhifeig@uw.edu>
2021-02-08 12:00:46 +0800

    google analytics

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ReactGA from 'react-ga';
 import App from './app';
+
+ReactGA.initialize('UA-47208480-12');
+ReactGA.pageview(window.location.pathname);
 
 ReactDOM.render(<App />, document.querySelector('#root'));
